5b90087731c039bf8118df43cbba0d71a419cef1
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2023-12-08 23:23:42 +0300

    order service and basket service added

---
+++ order-service/src/main/java/com/cengizhan/ecommerceproject/orderservice/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.cengizhan.ecommerceproject.orderservice.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
