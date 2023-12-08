e6e7b0a51950d6a6b910810fbfcdefe9688d3b69
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2023-12-08 23:24:07 +0300

    order service and basket service added

---
+++ basket-service/src/main/java/com/cengizhan/ecommerceproject/basketservice/BasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cengizhan.ecommerceproject.basketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketServiceApplication.class, args);
+	}
+
+}
---
+++ basket-service/src/test/java/com/cengizhan/ecommerceproject/basketservice/BasketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengizhan.ecommerceproject.basketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
