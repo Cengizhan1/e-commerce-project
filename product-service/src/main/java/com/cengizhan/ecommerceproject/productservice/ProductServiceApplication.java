3aac53ad8bca25536cd80f3e4b740b505cb51253
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-04-16 16:38:58 +0300

    pom.xml and docker-compose.yml updated for kafka

--- product-service/src/main/java/com/cengizhan/ecommerceproject/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/cengizhan/ecommerceproject/productservice/ProductServiceApplication.java
@@ -10,13 +10,13 @@ import feign.codec.ErrorDecoder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ProductServiceApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
